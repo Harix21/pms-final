dd0376fec6eb1348105ccb4d67ec8bc67b465bf8
Hariharan <hariharan.02050@gmail.com>
2026-05-15 21:17:21 +0530

    Integrate Dotenv for environment variable management
    Load environment variables using Dotenv and set them as system properties.

--- 76pro-backend/src/main/java/_pro_backend/Application.java
+++ 76pro-backend/src/main/java/_pro_backend/Application.java
@@ -1,5 +1,6 @@
 package _pro_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,15 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		dotenv.entries().forEach(e ->
+				System.setProperty(e.getKey(), e.getValue()));
+
+		SpringApplication.run(Application.class, args);
 	}
+
+
 
 }
